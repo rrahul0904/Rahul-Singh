e0c97d8320e39cad917af2ed74601338e5de8b7a
rrahul0904 <rrahul0904@users.noreply.github.com>
2026-04-03 23:04:19 -0400

    Add stable Unified Migration Accelerator frontend shell

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from "react";
+import UnifiedMigrationAcceleratorApp from "../../../canvas-export/UnifiedMigrationAcceleratorApp";
+
+export default function Page() {
+  return <UnifiedMigrationAcceleratorApp />;
+}
